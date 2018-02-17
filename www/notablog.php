ec75da21babd849e5bc7c05b033a9e048cc88c75
Christian Lück <christian@lueck.tv>
2018-02-17 17:27:53 +0100

    Support multiple posts by passing gist URL/ID to notablog.php

--- www/notablog.php
+++ www/notablog.php
@@ -3,13 +3,14 @@
 // notablog.php (C) 2018 by @clue
 
 // reject direct access with 403 (Forbidden)
-if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) !== false) {
+if (strpos($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']) !== false || !isset($_GET['gist'])) {
     header(' ', false, 403);
     exit('bad request.');
 }
 
 // thank you Gistlog, you're awesome! <3
-$url = 'https://gistlog.co/clue/8698d44e489927f9988b2a27e53d9189';
+$gist = str_replace('https://gist.github.com/', '', $_GET['gist']);
+$url = 'https://gistlog.co/' . $gist;
 $ret = @file_get_contents($url);
 if ($ret === false) {
     header(' ', false, 500);
@@ -26,6 +27,10 @@ $ret = preg_replace('#<a href="?/[^>]*>(.*?)</a>#is', '$1', $ret);
 $ret = preg_replace('#<title.*>([^\|]+).*</title>#i', '<title>$1</title>', $ret);
 
 // rewrite og:url to canonical target
-$ret = str_replace($url, 'https://www.lueck.tv/2018/hello-world', $ret);
+$ret = str_replace(
+    $url,
+    (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    $ret
+);
 
 echo $ret;
