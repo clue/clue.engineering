140a7543dc39e9d991fd9d772c69ab7753c44953
Christian Lück <christian@lueck.tv>
2018-02-18 19:44:25 +0100

    New blog post: Pushing the limits with ReactPHP

--- www/notablog.php
+++ www/notablog.php
@@ -18,13 +18,13 @@ if ($ret === false) {
 }
 
 // remove unneeded navigation bar
-$ret = preg_replace('#<nav.*>.*</nav>#is', '<br />', $ret);
+$ret = preg_replace('#<nav.*?>.*?</nav>#is', '<br />', $ret);
 
 // replace all root links with text
 $ret = preg_replace('#<a href="?/[^>]*>(.*?)</a>#is', '$1', $ret);
 
 // simplify title attribute
-$ret = preg_replace('#<title.*>([^\|]+).*</title>#i', '<title>$1</title>', $ret);
+$ret = preg_replace('#<title.*?>([^\|]+).*?</title>#i', '<title>$1</title>', $ret);
 
 // rewrite og:url to canonical target
 $ret = str_replace(
