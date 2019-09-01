e84a43e7dac59b6e7d19abdbe3609e96bf23124e
Christian Lück <christian@lueck.tv>
2019-09-01 14:53:45 +0200

    Implement contact form email submission

---
+++ www/contact.php
@@ -0,0 +1,31 @@
+<?php
+
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$message = filter_input(INPUT_POST, 'message', FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^.{20,}$/su']]);
+
+if (!is_string($email) || !is_string($message) || preg_match('/[\x00-\x08\x0b\x0c\x0e-\x1f]/', $message) || $_POST['url'] ?? '' !== '') {
+    header(' ', true, 400);
+    echo 'Invalid form submission, please check your data' . PHP_EOL;
+    exit(1);
+}
+
+$subject = 'Contact Christian Lück';
+$id = '<' . gmdate('YmdHis') . '.' . mt_rand() . '@clue.engineering>';
+$ret = mail('hello@clue.engineering', '=?UTF-8?B?' . base64_encode($subject) . '?=', $message, "From: hello@clue.engineering\r\nReply-To: $email\r\nMessage-ID: $id\r\nContent-Type: text/plain; charset=utf-8");
+assert($ret);
+
+$message = 'Thanks for reaching out!
+
+This is an automated reply to let you know your mail has made it my way and I will get back to your mail as soon as possible.
+
+Christian
+https://clue.engineering/
+
+
+Original message:
+> ' . preg_replace('/(\r?\n)/', "\r\n> ", trim($message));
+
+$ret = mail($email, '=?UTF-8?B?' . base64_encode('Re: ' . $subject) . '?=', $message, "From: hello@clue.engineering\r\nReferences: $id\r\nIn-Reply-To: $id\r\nContent-Type: text/plain; charset=utf-8");
+assert($ret);
+
+header('Location: contact#thanks');
